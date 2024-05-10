d5b351ef11f7bb89139f354e0a93f77e2c298a1f
cro2003 <chiragrai2003@icloud.com>
2024-05-11 01:45:02 +0530

    Added Index page in Flask

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import database as db
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    defaultLang = "en"
+    if request.method=='POST':
+        defaultLang = request.form['selectedLang']
+    return render_template('index.html', supportedLanguage = db.languageData['supportedLanguages'], pageLang = {"language": defaultLang, "codeToLang":  db.languageData["codeToLang"], "translatedData": db.languageData["translatedData"][defaultLang]["index"]})
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
---
+++ database.py
@@ -0,0 +1,51 @@
+languageData = {
+  "supportedLanguages": {
+    "English": "en",
+  },
+  "codeToLang": {
+    "en": "English",
+  },
+  "translatedData": {
+    "en": {
+      "index": {
+        "bb-logo": "Bharatभ्रमण",
+        "home": "Home",
+        "flight": "Flights",
+        "train": "Trains",
+        "hotel": "Hotels",
+        "multi-destination": "Multi Destinations",
+        "login": "Login",
+        "from": "Source",
+        "from-cities": ["Dubai", "Washington", "Los Angeles"],
+        "to": "Destination",
+        "to-cities": ["Delhi", "Agra", "Bhopal"],
+        "submit": "Submit",
+        "right_h1": "Life is Short and the World is Wide",
+        "delhi": "Delhi",
+        "kerala": "Kerala",
+        "mumbai": "Mumbai",
+        "jaipur": "Jaipur",
+        "agra": "Agra",
+        "kolkata": "Kolkata",
+        "goa": "Goa",
+        "varanasi": "Varanasi",
+        "sec": ["Places to", "Visit in", "India"],
+        "short-info-agra": "Agra, in northern India, is renowned for the iconic Taj Mahal, a symbol of eternal love. Home to historic forts like Agra Fort, it holds a rich Mughal architectural legacy.",
+        "short-info-delhi": "Delhi, India's capital, is a bustling metropolis with iconic landmarks such as India Gate and Qutub Minar. A melting pot of cultures, it showcases historical grandeur alongside modern developments.",
+        "short-info-kerala": "Kerala, known as Gods Own Country, features lush landscapes, backwaters, and pristine beaches. Its diverse culture, Ayurvedic traditions, and vibrant festivals make it a unique destination in southern India",
+        "short-info-mumbai": "Mumbai, India's financial hub, is a bustling metropolis with iconic landmarks like the Gateway of India. Known for Bollywood, diverse cuisine, and a vibrant lifestyle, it epitomizes dynamism.",
+        "short-info-jaipur": "Jaipur, the Pink City of India, is renowned for its vibrant culture, majestic palaces like Hawa Mahal and City Palace, and the historic Amer Fort. It exudes Rajasthani charm.",
+        "short-info-kolkata": "Kolkata, the cultural capital of India, boasts historical landmarks like Victoria Memorial and Howrah Bridge. Rich in literature, art, and cuisine, the city reflects a blend of tradition and modernity.",
+        "short-info-goa": "Goa, India's smallest state, is renowned for its vibrant culture, stunning beaches, and lively nightlife. With a mix of Portuguese influence, historic churches, and diverse cuisine, Goa attracts tourists worldwide.",
+        "short-info-varanasi": "Varanasi, a spiritual city in India, is renowned for its ancient ghats along the Ganges River. A center of Hindu pilgrimage, it exudes cultural richness with vibrant rituals, temples, and traditions.",
+        "bhopal": "Bhopal",
+        "long-info-bhopal": "Bhopal, the capital city of Madhya Pradesh, India, captivates tourists with its rich cultural heritage and scenic beauty. Explore the historic Taj-ul-Masajid, stroll through the lush Upper Lake, and visit the vibrant markets. Bhopal seamlessly blends tradition and modernity, offering a unique experience for those seeking history, art, and natural charm.",
+        "long-info-delhi": "Delhi, India's capital, is a vibrant metropolis blending history and modernity. Home to iconic landmarks like the Red Fort, India Gate, and Qutub Minar, it showcases a rich cultural tapestry. The bustling markets, diverse cuisine, and historical sites make Delhi a captivating destination, reflecting the nation's dynamic spirit.",
+        "errorFillBlnks": "Enter All the Required Details",
+        "errorInvldSrcDest": "Invalid Source or Destination",
+        "errorFlight404": "Flights Not Found",
+        "errorTrain404": "Trains Not Found"
+      }
+    }
+  }
+}
