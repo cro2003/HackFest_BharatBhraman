5fb910bd3d1a2f9563f90bc49cbf788923324c7b
cro2003 <chiragrai2003@icloud.com>
2024-05-11 11:03:24 +0530

    Added Mongo Db as database

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for, flash
 import database as db
 import os
 from dotenv import load_dotenv
+import threading
 from datetime import timedelta
 import flight
 import location
@@ -9,11 +10,13 @@ import train
 import hotel
 import json
 import utils
+import genai
 
 load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
 app.permanent_session_lifetime = timedelta(minutes=5)
+content = {}
 session = {}
 
 @app.route('/', methods=['GET', 'POST'])
@@ -36,6 +39,8 @@ def planTrip():
         return redirect(url_for('index'))
     date = [date.split('-')[2], date.split('-')[1], date.split('-')[0]]  # IN [DD, MM, YYYY]
     sessionId = utils.generateId(source, destination)
+    thread = threading.Thread(target=contentGen, args=(destData["city"],))
+    thread.start()
     currency = db.currency.get(sourceData['country_code'].upper())
     session[sessionId] = {"sourceData": sourceData, "destData": destData, "deptDate": date, "prefferedLang": prefferedLang, "currency": currency}
     if sourceData['country'] == "India":
@@ -106,110 +111,7 @@ def hotelDetails():
 def tripDetail():
     sessionId = request.args['sessionId']
     session[sessionId]['hotelData'] = json.loads(request.form['hotelData'].replace("'", '"'))
-    data = [
-            {
-                "placeName": "From Vihar National Park",
-                "description": "Van Vihar National Park is a popular tourist destination in Bhopal, known for its natural beauty and diverse wildlife. The park is home to a wide variety of animals, including tigers, leopards, bears, and various species of birds. Visitors can enjoy a safari through the park, or take a leisurely walk along the many trails. There are also several picnic areas and a children's playground.",
-                "address": "Van Vihar Road, Bhopal, Madhya Pradesh 462003",
-                "imageUrl": [
-                    "https://bharatstories.com/wp-content/uploads/2016/02/Van-Vihar-National-Parkbhopal.jpg",
-                    "https://gumlet.assettype.com/freepressjournal/2022-12/db7445ed-9c17-4321-9cac-610e88b4e460/van_vihar_national_park_bhopal__10_.jpg?rect=0%2C0%2C3900%2C2048&amp;w=1200&amp;auto=format%2Ccompress&amp;ogImage=true"
-                ],
-                "mapsDeeplink": "https://www.google.com/maps/search/?api=1&query=Van%20Vihar%20National%20Park%20Bhopal"
-            },
-            {
-                "placeName": "Bhojtal",
-                "description": "Bhojtal is a large lake located in the heart of Bhopal. The lake is a popular spot for boating, fishing, and bird watching. There are also several restaurants and cafes located along the shore of the lake. Visitors can enjoy a leisurely walk around the lake, or take a boat ride to explore the many islands.",
-                "address": "Bhojtal, Bhopal, Madhya Pradesh 462038",
-                "imageUrl": [
-                    "https://c8.alamy.com/comp/H683E9/bhojtal-is-a-large-lake-which-lies-on-the-western-side-of-the-capital-H683E9.jpg",
-                    "https://i.pinimg.com/originals/96/27/82/962782e4c922d828f91caf9eae176f00.jpg"
-                ],
-                "mapsDeeplink": "https://www.google.com/maps/search/?api=1&query=Bhojtal%20Bhopal"
-            },
-            {
-                "placeName": "Upper Lake",
-                "description": "The Upper Lake is a large lake located in the northern part of Bhopal. The lake is a popular spot for boating, fishing, and swimming. There are also several temples and mosques located along the shore of the lake. Visitors can enjoy a leisurely walk around the lake, or take a boat ride to explore the many islands.",
-                "address": "Upper Lake, Bhopal, Madhya Pradesh 462003",
-                "imageUrl": [
-                    "https://www.wheelsonourfeet.com/wp-content/uploads/2016/02/Upper-Lake-in-Bhopal-1.jpg",
-                    "https://www.trawell.in/admin/images/upload/145239155Upper-Lake.jpg"
-                ],
-                "mapsDeeplink": "https://www.google.com/maps/search/?api=1&query=Upper%20Lake%20Bhopal"
-            },
-            {
-                "placeName": "Lower Lake",
-                "description": "The Lower Lake is a small lake located in the southern part of Bhopal. The lake is a popular spot for boating, fishing, and swimming. There are also several temples and mosques located along the shore of the lake. Visitors can enjoy a leisurely walk around the lake, or take a boat ride to explore the many islands.",
-                "address": "Lover Lake, Bhopal, Madhya Pradesh 462003",
-                "imageUrl": [
-                    "https://cdn1.goibibo.com/voy_ing/t_fs/bhopal-lower-lake-157387635624o.jpeg",
-                    "https://images.herzindagi.info/image/2021/Jun/Bhopal-lake.jpg"
-                ],
-                "mapsDeeplink": "https://www.google.com/maps/search/?api=1&query=Lower%20Lake%20Bhopal"
-            },
-            {
-                "placeName": "Taj-ul-Masjid",
-                "description": "Taj-ul-Masjid is a large mosque located in the heart of Bhopal. The mosque is one of the largest mosques in India, and is a popular tourist destination. Visitors can admire the intricate architecture of the mosque, or take a guided tour to learn about its history.",
-                "address": "Taj-ul-Masjid Road, Bhopal, Madhya Pradesh 462001",
-                "imageUrl": [
-                    "https://cdn.theculturetrip.com/wp-content/uploads/2016/04/tajul-masajid.jpeg",
-                    "https://www.holidaytravel.co/uploaded_files/destination_img/2016_3857efa2d2e5b3bthe-tajul-masjid-bhopal-tourist-guide.jpg"
-                ],
-                "mapsDeeplink": "https://www.google.com/maps/search/?api=1&query=Taj-ul-Masjid%20Bhopal"
-            },
-            {
-                "placeName": "Shaukat Mahal",
-                "description": "Shaukat Mahal is a palace located in the heart of Bhopal. The palace was built in the 19th century, and is a popular tourist destination. Visitors can admire the intricate architecture of the palace, or take a guided tour to learn about its history.",
-                "address": "Shaukat Mahal Road, Bhopal, Madhya Pradesh 462001",
-                "imageUrl": [
-                    "https://www.trawell.in/admin/images/upload/145239855Bhopal_Shaukat_Mahal_Main.jpg",
-                    "https://www.hoteldekho.com/storage/img/tourattraction/1646731104Shaukat Mahal.jpg"
-                ],
-                "mapsDeeplink": "https://www.google.com/maps/search/?api=1&query=Shaukat%20Mahal%20Bhopal"
-            },
-            {
-                "placeName": "Gohar Mahal",
-                "description": "Gohar Mahal is a palace located in the heart of Bhopal. The palace was built in the 19th century, and is a popular tourist destination. Visitors can admire the intricate architecture of the palace, or take a guided tour to learn about its history.",
-                "address": "Gauhar Mahal Road, Bhopal, Madhya Pradesh 462001",
-                "imageUrl": [
-                    "https://www.trawell.in/admin/images/upload/145239559Bhopal_Gohar_Mahal_Main.jpg",
-                    "https://www.re-thinkingthefuture.com/wp-content/uploads/2020/06/A1008-Places-to-visit-in-Bhopal-for-the-Travelling-Architect-Gohar-Mahal-Image-1.jpg"
-                ],
-                "mapsDeeplink": "https://www.google.com/maps/search/?api=1&query=Gohar%20Mahal%20Bhopal"
-            },
-            {
-                "placeName": "Human Museum.",
-                "description": "Manav Sangrahalaya is a museum located in the heart of Bhopal. The museum is dedicated to the anthropology of India, and houses a large collection of artifacts from all over the country. Visitors can learn about the diverse cultures of India, and see how they have changed over time.",
-                "address": "Manav Sangrahalaya Road, Bhopal, Madhya Pradesh 462003",
-                "imageUrl": [
-                    "https://www.tourismnewslive.com/wp-content/uploads/2018/04/odisha.jpg",
-                    "https://holidify.com/images/attr_wiki/compressed/attr_wiki_3487.jpg",
-                    "https://www.trawell.in/admin/images/upload/145239409Indiragandhimanav_sangrahalaya.jpg"
-                ],
-                "mapsDeeplink": "https://www.google.com/maps/search/?api=1&query=Manav%20Sangrahalaya%20Bhopal"
-            },
-            {
-                "placeName": "Bhimbetka Caves",
-                "description": "Bhimbetka Caves are a group of rock shelters located on the outskirts of Bhopal. The caves are home to a large number of prehistoric paintings, which date back to the Stone Age. Visitors can explore the caves, and see the paintings for themselves.",
-                "address": "Bhimbetka Caves Road, Bhopal, Madhya Pradesh 462038",
-                "imageUrl": [
-                    "https://www.indiatravelblog.net/wp-content/uploads/2011/02/Bhimbetka-Caves.jpg",
-                    "https://static.wixstatic.com/media/689c7e_001049cd7b65452abc356c32218db5a9~mv2.jpg/v1/fill/w_1000,h_500,al_c,q_90,usm_0.66_1.00_0.01/689c7e_001049cd7b65452abc356c32218db5a9~mv2.jpg"
-                ],
-                "mapsDeeplink": "https://www.google.com/maps/search/?api=1&query=Bhimbetka%20Caves%20Bhopal"
-            },
-            {
-                "placeName": "Sanchi Stupa",
-                "description": "Sanchi Stupa is a Buddhist stupa located on the outskirts of Bhopal. The stupa was built in the 3rd century BC, and is a popular tourist destination. Visitors can admire the intricate carvings on the stupa, or take a guided tour to learn about its history.",
-                "address": "Sanchi Stupa Road, Bhopal, Madhya Pradesh 462038",
-                "imageUrl": [
-                    "https://www.holidify.com/images/cmsuploads/compressed/sanchistupa_20180219185128.jpg",
-                    "https://www.esamskriti.com/essays/docfile/42_3746.jpg"
-                ],
-                "mapsDeeplink": "https://www.google.com/maps/search/?api=1&query=Sanchi%20Stupa%20Bhopal"
-            }
-        ]
-    #data = content[destinationData["city"]]
+    data = content[session[sessionId]['destData']["city"]]
     data = {"sourceData": session[sessionId]['sourceData'], "destinationData": session[sessionId]['destData'], "deptDate": f"{session[sessionId]['deptDate'][0]}-{session[sessionId]['deptDate'][1]}-{session[sessionId]['deptDate'][2]}", "hotelData": session[sessionId]['hotelData'], "guideDetails": db.guideDetails["en"], "content": data, "currency": session[sessionId]['currency']}
     if session[sessionId].get('flightData')!=None:
         data["flightData"] = session[sessionId]['flightData']
@@ -225,11 +127,10 @@ def tripDetail():
                            pageLang={"language": session[sessionId]['prefferedLang'], "codeToLang": db.languageData["codeToLang"],
                                      "translatedData": db.languageData["translatedData"][session[sessionId]['prefferedLang']]["chooseGuide"]})
 
-
-
-
-
-
+def contentGen(city):
+    data = genai.contentCreator(city)
+    content[city] = data
+    print("Content Generated!")
 
 if __name__ == "__main__":
     app.run()
--- genai.py
+++ genai.py
@@ -2,16 +2,36 @@ import json
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+from imageSearcher import ImageSearcher
+import database as db
+import threading
 
 load_dotenv()
 
 genai.configure(api_key=os.getenv('GEMINI_PRO'))
 model = genai.GenerativeModel('gemini-pro')
 
-def generate_content(city):
-    content = model.generate_content('Give 10 Places to Visit in '+ city +' in given json format {places:[{"placeName":  PLACE_NAME, "description":  DETAILED_DESCRIPTION_WITH_AT_LEAST_30_WORDS, "address": FULL_ADDRESS_OF_THE_PLACE}]}', generation_config=genai.types.GenerationConfig(max_output_tokens=2048))
+def contentCreator(city, forceGen=False):
+    threads = []
+    if not forceGen and db.content.get(city) != None:
+        return
+    content = model.generate_content('Give 10 Places to Visit in ' + city + ' in given json format {places:[{"placeName":  PLACE_NAME, "description":  DETAILED_DESCRIPTION_WITH_AT_LEAST_30_WORDS, "address": FULL_ADDRESS_OF_THE_PLACE}]}',generation_config=genai.types.GenerationConfig(max_output_tokens=2048))
     content.resolve()
     data = content.text[content.text.find('{'):]
     data = data[:data.rfind('}') + 1]
     data = json.loads(data)
-    return data
+    db.content[city] = data['places']
+    for index in range(len(db.content[city])):
+        thread = threading.Thread(target=searchImage, args=(db.content[city][index]['placeName'] + " " + city, city, index))
+        thread.start()
+        threads.append(thread)
+        searchFormat = (db.content[city][index]['placeName'] + " " + city).replace(' ', '%20')
+        db.content[city][index]["mapsDeeplink"] = "https://www.google.com/maps/search/?api=1&query=" + searchFormat
+        print(db.content[city][index])
+    for thread in threads:
+        thread.join()
+    return db.content[city]
+
+def searchImage(query, city, index):
+    image = ImageSearcher(query)
+    db.content[city][index]["imageUrl"] = image.run()
